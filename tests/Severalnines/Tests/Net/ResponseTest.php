5f5bc1e00330ee97bdf8d166ab8fd22ed49be2d8
Semen Kamenetskiy <skamenetskiy@live.com>
2016-03-21 10:10:18 +0300

    clustercontrol-rpcphp-1 Write test case for Severalnines\Rpc\Net\Request

---
+++ tests/Severalnines/Tests/Net/ResponseTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Severalnines\Tests\Net;
+
+use Severalnines\Tests\Test;
+
+/**
+ * Class ResponseTest
+ *
+ * @package Severalnines\Tests\Net
+ */
+class ResponseTest
+    extends Test
+{
+
+    public function testConstructor()
+    {
+    }
+
+}
