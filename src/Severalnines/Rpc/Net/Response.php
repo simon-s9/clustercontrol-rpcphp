6fdbbadea0bd314a58fa71e478a29f98fd9ee674
Semen Kamenetskiy <skamenetskiy@live.com>
2016-05-20 12:30:58 +0300

    Added getJobs() and getRawResponse() methods to Response class

--- src/Severalnines/Rpc/Net/Response.php
+++ src/Severalnines/Rpc/Net/Response.php
@@ -19,6 +19,11 @@ class Response
     /**
      * @var array
      */
+    private $_jobs = array();
+
+    /**
+     * @var array
+     */
     private $_clusters = array();
 
     /**
@@ -35,6 +40,11 @@ class Response
      * @var string
      */
     private $_error = null;
+
+    /**
+     * @var array
+     */
+    private $_rawResponse = null;
 
     /**
      * Response constructor.
@@ -54,6 +64,9 @@ class Response
         if (array_key_exists('data', $rpcResponse)) {
             $this->_data = $rpcResponse['data'];
         }
+        if (array_key_exists('jobs', $rpcResponse)) {
+            $this->_jobs = $rpcResponse['jobs'];
+        }
         if (array_key_exists('clusters', $rpcResponse)) {
             $this->_clusters = $rpcResponse['clusters'];
         }
@@ -66,6 +79,15 @@ class Response
         if (array_key_exists('errorString', $rpcResponse)) {
             $this->_error = $rpcResponse['errorString'];
         }
+        $this->_rawResponse = $rpcResponse;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRawResponse()
+    {
+        return $this->_rawResponse;
     }
 
     /**
@@ -88,6 +110,14 @@ class Response
      * @return array
      */
     public function getClusters()
+    {
+        return $this->_clusters;
+    }
+
+    /**
+     * @return array
+     */
+    public function getJobs()
     {
         return $this->_clusters;
     }
